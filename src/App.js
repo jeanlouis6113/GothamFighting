2c58770bf72eb78ae38b333cf3fa675e9f0cae6e
William <will142@hotmail.fr>
2020-04-22 16:56:54 +0200

    creation du component et intégration à la page d'acceuil

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import ButtonBio from './components/ButtonBio'
 import ButtonPlay from './components/ButtonPlay'
 import PageBio from './components/PageBio'
 import PageCombat from './components/PageCombat'
+import Carrousel from './components/Carrousel'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Logo />
-      <ButtonBio />
-      <ButtonPlay />
       <Switch>
-        <Route exact path="/" />
+        <Route exact path="/" component={Carrousel}/>
         <Route exact path="/PageBio" component={PageBio} />
         <Route path="/PageCombat" component={PageCombat} />
       </Switch>
+      <ButtonBio />
+      <ButtonPlay />
     </div>
     
   );
