9cb821bd20208806b2be3e5b81dc1749682afd15
ashe8408 <anael.b.d@hotmail.fr>
2020-04-23 16:15:13 +0200

    MAJ layout page accueil

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch, } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
 import Logo from './components/Logo';
 import Carrousel from './components/Carrousel'
 import ButtonBio from './components/ButtonBio';
@@ -10,20 +11,54 @@ import PageCombat from './components/PageCombat';
 import Video from './components/Video';
 import './App.css';
 
+/*
+Composant vidéo retiré en attente : 
+
+          <Grid container direction="row" justify="center">
+            <Grid item xs={0}>
+              <Video/>
+            </Grid>
+          </Grid>
+
+*/
+
+
 function App() {
   return (
     <div className="App">
-      <Logo />
-      <Carrousel />
-      <ButtonBio />
-      <ButtonPlay />
-      <Video/>
-      <ButtonSettings />
-      <Switch>
-        <Route exact path="/" />
-        <Route exact path="/PageBio" component={PageBio} />
-        <Route path="/PageCombat" component={PageCombat} />
-      </Switch>
+        <Grid container direction="column">
+          <Grid container item xs={1}>
+            <Logo />
+          </Grid>
+          
+          <Grid container justify="center">
+            <Grid item xs={9}>
+              <Carrousel />
+            </Grid>
+          </Grid>
+      
+          <Grid container direction="row" justify="center">
+            <Grid>
+              <ButtonBio />
+            </Grid>
+            <Grid>
+              <ButtonPlay />
+            </Grid>
+          </Grid>
+          
+          <Grid container justify="flex-end">
+            <Grid item xs={1}>
+              <ButtonSettings />
+            </Grid>
+          </Grid>
+          
+          
+        </Grid>
+        <Switch>
+            <Route exact path="/" />
+            <Route exact path="/PageBio" component={PageBio} />
+            <Route path="/PageCombat" component={PageCombat} />
+        </Switch>
     </div>
   );
 }
--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -3,22 +3,26 @@ import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
 import 'react-awesome-slider/dist/styles.css';
 import './CaptionedStyles.scss';
+import Video from './Video';
 import { Link } from 'react-router-dom';
 
 
 function Carrousel() {
     return (
         <Link to="/">  
-            <AwesomeSlider animation="cubeAnimation" className="slider-homepage">
-            <div data-src="./images/batmanCarrousel.jpeg">
-                <p>BATMAN</p>
-            </div>
-            <div data-src="./images/theJokerCarrousel.webp">
-                <p>THE JOKER</p>
-            </div>
-            <div data-src="./images/thePenguinCarrousel.jpeg">
-                <p>THE PENGUIN</p>
-            </div>   
+            <AwesomeSlider animation="cubeAnimation" className="slider-homepage"> 
+                <div data-src="./images/batmanCarrousel.jpeg">
+                    <p>BATMAN</p>
+                </div>
+                <div>
+                    <Video />
+                </div>
+                <div data-src="./images/theJokerCarrousel.webp">
+                    <p>THE JOKER</p>
+                </div>
+                <div data-src="./images/thePenguinCarrousel.jpeg">
+                    <p>THE PENGUIN</p>
+                </div>  
             </AwesomeSlider>
         </Link>
     );
