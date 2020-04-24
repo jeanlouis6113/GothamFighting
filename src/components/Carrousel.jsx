e93647a09e494c81f3ef54f5735c48da642c68de
jeanlouis <59745606+jeanlouis6113@users.noreply.github.com>
2020-04-24 10:25:10 +0200

    intégréation de la bon son et du contenu cacher

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -9,20 +9,22 @@ import { Link } from 'react-router-dom';
 
 function Carrousel() {
     return (
-        <Link to="/">  
-            <AwesomeSlider animation="cubeAnimation" className="slider-homepage"> 
+        <Link to="/">
+            <AwesomeSlider animation="cubeAnimation" className="slider-homepage" >
                 <div data-src="./images/batmanCarrousel.jpeg">
                     <p>BATMAN</p>
                 </div>
                 <div>
-                    <Video />
+                    <img id="imageBonus" src="./images/HommeMystere.png" alt=""/>
+                    <Video className="masquer"/>
                 </div>
+
                 <div data-src="./images/theJokerCarrousel.webp">
                     <p>THE JOKER</p>
                 </div>
                 <div data-src="./images/thePenguinCarrousel.jpeg">
                     <p>THE PENGUIN</p>
-                </div>  
+                </div>
             </AwesomeSlider>
         </Link>
     );
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,18 +3,25 @@ import { Media, Player } from 'react-media-player';
 import "./Video.css";
 
 class Video extends Component {
+
   render() {
     return (
-      <Media >
-        <div className="media">
-          <div className="media-player ">
-            <Player src="https://www.youtube.com/watch?v=W8RamqJ0BgI&t=3s" 
-            /> 
-          </div>
-          <div className="media-controls">
-          </div>
+      <div >
+
+        <div className="masquer">
+          <Media>
+            <div className="media">
+              <div className="media-player ">
+                <Player src="https://www.youtube.com/watch?v=W8RamqJ0BgI&t=3s"
+                />
+              </div>
+              <div className="media-controls">
+              </div>
+            </div>
+          </Media>
         </div>
-      </Media>
+
+      </div>
     )
   }
 }
