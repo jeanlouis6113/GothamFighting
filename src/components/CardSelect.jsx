95e4c01d01a1ce5b694bae2b545ae282e47f8867
ashe8408 <anael.b.d@hotmail.fr>
2020-04-28 12:58:58 +0200

    Page Bio Amelioree

--- src/components/CardSelect.jsx
+++ src/components/CardSelect.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { Grid } from '@material-ui/core';
 
 
 class CardSelect extends Component{
@@ -7,9 +11,21 @@ class CardSelect extends Component{
         const {nameCharacter, urlImage} = this.props;
         console.log("Cards props: ", nameCharacter, urlImage);
         return (
-            <div>
-                <img src={urlImage} alt={nameCharacter} className="avatarBio"/>
-            </div>
+            <Grid container direction="row">
+                    <CardActionArea>
+                        <Grid container direction="row" justify="center" alignItems="center">
+                            <Grid item xs={6}>
+                                <img src ={urlImage}  alt="" className="avatarBio"/>
+                            </Grid>    
+                        </Grid> 
+                        <CardContent className="avatarName">
+                            <Typography gutterBottom="true" align="center" variant="h4">
+                                {nameCharacter}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+            </Grid>
+            
         )
     }
 }
