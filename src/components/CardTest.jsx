43834d100bf3eebca57263e1d6d8cafc6f15e260
ashe8408 <anael.b.d@hotmail.fr>
2020-04-28 11:08:03 +0200

    Page Bio fonctionnelle

--- src/components/CardTest.jsx
+++ src/components/CardTest.jsx
@@ -3,66 +3,73 @@ import React, { Component } from 'react';
 class CardTest extends Component{
     constructor(props) {
         super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            urlImage:'/images/logoGotham.svg',
-            nameCharacter: 'character'
-        };
+        // this.state = {
+        //     error: null,
+        //     isLoaded: false,
+        //     urlImage:'/images/logoGotham
+        //     nameCharacter: 'character'
+        // };
     }
 
-    componentDidUpdate() {
-        console.log("Props Update:", this.props);
-        if (!this.state.isLoaded) {
-            this.loadImage(this.props.charactersId);
-        }
-    }
+    // componentDidUpdate() {
+    //     console.log("Props Update:", this.props);
+    //     if (!this.state.isLoaded) {
+    //         this.loadImage(this.props.charactersId);
+    //     }
+    // }
 
-    componentDidMount() {
-        console.log("Props Mount:", this.props);
-        this.loadImage(this.props.charactersId);
-    }
+    // componentDidMount() {
+    //     console.log("Props Mount:", this.props);
+    //     this.loadImage(this.props.charactersId);
+    // }
 
-    loadImage() {
-        let url = `https://www.superheroapi.com/api.php/108881107445202/${this.props.charactersId}`;
-        fetch(url)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    this.setState({
-                        isLoaded: true,
-                        urlImage: result.image.url,
-                        nameCharacter: result.name
-                    });
-                },
-                (error) => {
-                    this.setState({
-                    isLoaded: true,
-                    error
-                    });
-                }
-            )
-    }
+    // loadImage() {
+    //     let url = `https://www.superheroapi.com/api.php/108881107445202/${this.props.charactersId}`;
+    //     fetch(url)
+    //         .then(res => res.json())
+    //         .then(
+    //             (result) => {
+    //                 this.setState({
+    //                     isLoaded: true,
+    //                     urlImage: result.image.url,
+    //                     nameCharacter: result.name
+    //                 });
+    //             },
+    //             (error) => {
+    //                 this.setState({
+    //                 isLoaded: true,
+    //                 error
+    //                 });
+    //             }
+    //         )
+    // }
 
     render() {
-        const {isLoaded, urlImage, nameCharacter, error} = this.state;
-        if (error) {
-            console.log("url fetched:", urlImage)
-            return (
-                <div>
-                    <p>Erreur : {error.message}</p>
-                    <img src={urlImage} alt={nameCharacter} id="avatarBio"/>
-                </div>)
-        } else if (!isLoaded) {
-            return <img src="/images/gothamPlaceholder.jpg" alt={nameCharacter} className="avatarBio"/>;
-        } else {
-            console.log("url", urlImage)
-            return (
-                <div>
-                    <img src={urlImage} alt={nameCharacter} className="avatarBio"/>
-                </div>
-            );
-        }
+        // const {isLoaded, urlImage, nameCharacter, error} = this.state;
+        const {nameCharacter, urlImage} = this.props;
+        console.log("Cards props: ", nameCharacter, urlImage);
+        // if (error) {
+        //     console.log("url fetched:", urlImage)
+        //     return (
+        //         <div>
+        //             <p>Erreur : {error.message}</p>
+        //             <img src={urlImage} alt={nameCharacter} id="avatarBio"/>
+        //         </div>)
+        // } else if (!isLoaded) {
+        //     return <img src="/images/gothamPlaceholder.jpg" alt={nameCharacter} className="avatarBio"/>;
+        // } else {
+        //     console.log("url", urlImage)
+        //     return (
+        //         <div>
+        //             <img src={urlImage} alt={nameCharacter} className="avatarBio"/>
+        //         </div>
+        //     );
+        // }
+        return (
+            <div>
+                <img src={urlImage} alt={nameCharacter} className="avatarBio"/>
+            </div>
+        )
     }
 }
 
--- src/components/PersonnageList.jsx
+++ src/components/PersonnageList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import characters from './characters.json';
 import CardTest from './CardTest';
@@ -6,13 +6,52 @@ import { Grid } from '@material-ui/core';
 
 
 function PersonnageList(props) {
+  const [apiCharacters, setApiCharacters] = useState([]);
+
+  async function fetchData() {
+    for (let i = 0; i < characters.length; i++) {
+      let url = `https://www.superheroapi.com/api.php/1109560066088421/${characters[i].id}`;
+      const res = await fetch(url)
+        .then(res => {
+          if(!res.ok) {
+            throw new Error(res.status);
+          } else {
+            console.log("ok");
+            return res.json();
+          }
+        })
+        .then(
+          (result) => {
+            console.log("result: ", result);
+            if (result.image.url !== undefined) {
+              const character = {
+                id: characters[i].id,
+                urlImage: result.image.url,
+                nameCharacter: result.name
+              };
+              setApiCharacters(oldArray => [...oldArray,character]);
+            }
+          }
+        )
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+
+  useEffect(() => {
+      fetchData();
+    },
+    []
+  );
+
   return (
     <div className="CharacterList">
       <Grid container direction="row" justify="center" wrap="wrap" spacing={2}>
-          {characters.map(character => (
-          <Grid item sm={11} md={5} lg={4} key={characters.id} className="Character">
+          {apiCharacters.map(character => (
+          <Grid item sm={11} md={5} lg={4} key={character.id} className="Character">
               <Link to={`/PageBio/characters/${character.id}`}>
-                  <CardTest key={character.id} charactersId={character.id}/>
+                  <CardTest key={character.id} {...character}/>
               </Link>
           </Grid>
         ))}
