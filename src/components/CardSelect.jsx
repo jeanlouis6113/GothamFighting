d8521fd1dcaf3d5a1c89b07ec3e192db6811dc4a
jeanlouis <59745606+jeanlouis6113@users.noreply.github.com>
2020-05-14 17:01:47 +0200

    amélioration avant d'ajouter une fonctionnalité

--- src/components/CardSelect.jsx
+++ src/components/CardSelect.jsx
@@ -3,6 +3,7 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core';
+// import './HoverImage.scss';
 
 
 class CardSelect extends Component {
@@ -22,11 +23,11 @@ class CardSelect extends Component {
             <Grid container direction="row">
                 <CardActionArea>
                     <CardContent className="avatarName">
-                        <Typography gutterBottom="true" align="center" variant="h4">
+                        <Typography gutterBottom="true" align="center" variant="h4" className="infocard">
                             {nameCharacter}
                         </Typography>
                     </CardContent>
-                    <Grid container direction="row" justify="center" alignItems="center">
+                    <Grid container direction="row" justify="center" alignItems="center" className="cardselected">
                         <Grid item xs={6}>
                             <img src={urlImage} alt="" className="avatarBio" />
                         </Grid>
